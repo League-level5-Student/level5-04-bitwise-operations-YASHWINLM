9f784c76d31d5c96f2753cbeb8f1e991753eb019
League Student <league@iMac-18.attlocal.net>
2019-12-29 17:24:12 -0800

    finish base64 decoder

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -7,19 +7,44 @@ public class BinaryPrinter {
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
 	
-	public void printByteBinary(byte b) {
 	
+	public static void main(String[] args) {
+		BinaryPrinter bp= new BinaryPrinter();
+		bp.printByteBinary((byte) 64);
+		System.out.println("/n");
+		bp.printShortBinary((short) 12);
+		System.out.println("/n");
+		bp.printIntBinary(4);
+		System.out.println("/n");
+		bp.printLongBinary(8);
+		
+	}
+	
+	public void printByteBinary(byte b) {
+	System.out.print((b&0b10000000)>>7);
+	System.out.print((b&0b01000000)>>6);
+	System.out.print((b&0b00100000)>>5);
+	System.out.print((b&0b00010000)>>4);
+	System.out.print((b&0b00001000)>>3);
+	System.out.print((b&0b00000100)>>2);
+	System.out.print((b&0b00000010)>>1);
+	System.out.print((b&0b00000001)>>0);
+		
 	}
 	
 	public void printShortBinary(short s) {
+		printByteBinary((byte)s);
+		
 		
 	}
 	
 	public void printIntBinary(int i) {
-		
+		printByteBinary((byte)i);
 	}
 	
 	public void printLongBinary(long l) {
-		
+		printByteBinary((byte)l);
 	}
+	
+	
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -37,14 +37,24 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		int x=0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if (base64Chars[i]==c) {
+				x=i;
+			}
+		}
+		
+		
+		return  base64Chars[x];
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		return null;
+		char[] characters=s.toCharArray();
+		
+		return characters[];
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
